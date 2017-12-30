c44ad92143a9ecdc6bf2d9afcde4115fcf1a70ac
Peter Stimpel <pstimpel+github@gmail.com>
2017-12-30 17:09:12 +0100

    Mindmap (Freemind) -some basic thoughts and some order to the chaos

---
+++ Mindmap/SnowdenAngels.mm
@@ -0,0 +1,106 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1514648834486" ID="ID_649291143" MODIFIED="1514649122640" TEXT="Snowden Angels">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1514648887283" ID="ID_1369415457" MODIFIED="1514649122626" POSITION="right" TEXT="xmr-stak">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514648900617" ID="ID_930778869" MODIFIED="1514649122627" TEXT="Contact maintener and find a way to reduce maintainers fee">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514648931301" ID="ID_308846184" MODIFIED="1514649122630" TEXT="How to maintain a xmr-stak-clone - if needed?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1514648956157" ID="ID_1511640378" MODIFIED="1514649122632" POSITION="left" TEXT="Create a bullet-proof setup">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514648972981" ID="ID_1623956030" MODIFIED="1514649122633" TEXT="Ask for parameters">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1514649024267" ID="ID_1521208262" MODIFIED="1514649122635" TEXT="Autostart yes/no">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1514649051008" ID="ID_143444321" MODIFIED="1514649122637" POSITION="right" TEXT="Desktop Client">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514649061008" ID="ID_1400178043" MODIFIED="1514649122638" TEXT="Should show some nifty stats">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649072552" ID="ID_329484595" MODIFIED="1514649122639" TEXT="Should give opportunity to brag with the support">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649089120" ID="ID_1754962058" MODIFIED="1514649122640" TEXT="Should handle updates of both xmr-stak and the client">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514648982114" ID="ID_94253989" MODIFIED="1514649980338" TEXT="# of cpu cores to use">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514648994353" ID="ID_397664726" MODIFIED="1514649989786" TEXT="Give advice which pool to use, to not put all supporters to one pool">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649024267" ID="ID_48455026" MODIFIED="1514650001835" TEXT="Autostart yes/no">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1514649156418" ID="ID_1073592987" MODIFIED="1514649165522" POSITION="left" TEXT="official support site">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514649167441" ID="ID_319360227" MODIFIED="1514649197627" TEXT="Needs a public usable XMR address, without the need of an payment id (no exchange address)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649204304" ID="ID_123659340" MODIFIED="1514649222433" TEXT="Should explain the project, goals, and people behind">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649227435" ID="ID_56778627" MODIFIED="1514649244271" TEXT="Should offer basic support contacts for both technics and payments">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1514649262059" ID="ID_1195678914" MODIFIED="1514649269560" POSITION="right" TEXT="Github">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514649271802" ID="ID_1996695231" MODIFIED="1514649285559" TEXT="To provide sources and support contacts">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1514649336829" ID="ID_1347767414" MODIFIED="1514649341114" POSITION="left" TEXT="Stats page">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1514649342869" ID="ID_90090936" MODIFIED="1514649348417" TEXT="How many supporters">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649351127" ID="ID_74473220" MODIFIED="1514649361642" TEXT="How much Hashrate at the moment">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649364743" ID="ID_135043432" MODIFIED="1514649378020" TEXT="How much hashrate total">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649383110" ID="ID_1491073529" MODIFIED="1514649395460" TEXT="How many Moneros total">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1514649399724" ID="ID_153266142" MODIFIED="1514649418206" TEXT="Maybe a list of supporters, needs username inside stats">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
