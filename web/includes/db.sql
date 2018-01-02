d87d9fc4b1cb87ef55140a92c7e937ddea58f2ea
Peter Stimpel <pstimpel+github@gmail.com>
2018-01-02 01:16:25 +0100

    DB schemata

---
+++ web/includes/db.sql
@@ -0,0 +1,102 @@
+-- Table: public.errors
+
+-- DROP TABLE public.errors;
+
+CREATE TABLE public.errors
+(
+    errors_id integer NOT NULL DEFAULT nextval('errors_errors_id_seq'::regclass),
+    errors_ts_created timestamp without time zone NOT NULL,
+    errors_ts_inserted timestamp without time zone NOT NULL,
+    errors_sourceip text COLLATE pg_catalog."default" NOT NULL,
+    errors_programversion character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    errors_message text COLLATE pg_catalog."default" NOT NULL,
+    errors_stacktrace text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT errors_pkey PRIMARY KEY (errors_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.errors
+    OWNER to dbsgasupport;
+
+-- Index: idx_errors_errors_message
+
+-- DROP INDEX public.idx_errors_errors_message;
+
+CREATE INDEX idx_errors_errors_message
+    ON public.errors USING btree
+    (errors_message COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+
+-- Index: idx_errors_errors_programversion
+
+-- DROP INDEX public.idx_errors_errors_programversion;
+
+CREATE INDEX idx_errors_errors_programversion
+    ON public.errors USING btree
+    (errors_programversion COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+
+-- Index: idx_errors_errors_stacktrace
+
+-- DROP INDEX public.idx_errors_errors_stacktrace;
+
+CREATE INDEX idx_errors_errors_stacktrace
+    ON public.errors USING btree
+    (errors_stacktrace COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+	
+	
+	
+-- Table: public.stats_persession
+
+-- DROP TABLE public.stats_persession;
+
+CREATE TABLE public.stats_persession
+(
+    stats_persession_id integer NOT NULL DEFAULT nextval('stats_persession_stats_persession_id_seq'::regclass),
+    stats_persession_userkey character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    stats_persession_sessionstart timestamp without time zone NOT NULL,
+    stats_persession_sessionend timestamp without time zone NOT NULL,
+    stats_persession_hashes integer NOT NULL,
+    stats_persession_computerkey character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT stats_persession_pkey PRIMARY KEY (stats_persession_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.stats_persession
+    OWNER to dbsgasupport;
+
+-- Index: idx_stats_persession_computerkey
+
+-- DROP INDEX public.idx_stats_persession_computerkey;
+
+CREATE INDEX idx_stats_persession_computerkey
+    ON public.stats_persession USING btree
+    (stats_persession_computerkey COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+
+-- Index: idx_stats_persession_sessionstart
+
+-- DROP INDEX public.idx_stats_persession_sessionstart;
+
+CREATE INDEX idx_stats_persession_sessionstart
+    ON public.stats_persession USING btree
+    (stats_persession_sessionstart)
+    TABLESPACE pg_default;
+
+-- Index: idx_stats_persession_userkey
+
+-- DROP INDEX public.idx_stats_persession_userkey;
+
+CREATE INDEX idx_stats_persession_userkey
+    ON public.stats_persession USING btree
+    (stats_persession_userkey COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
+	
+	
