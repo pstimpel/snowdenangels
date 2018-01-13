4b2df69d8503f98b57ec7b7db589627ea9c0eb3e
Peter Stimpel <pstimpel+github@gmail.com>
2018-01-14 00:32:53 +0100

    Sitemap added to make the web crawler friendly

---
+++ web/sitemap.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-type: application/xhtml+xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";?>
+<urlset xmlns="http://www.google.com/schemas/sitemap/0.84">
+    <url>
+        <loc>https://redzoneaction.org/sgasupport/</loc>
+        <lastmod><?php echo date("Y-m-d")?></lastmod>
+    </url>
+    <url>
+        <loc>https://redzoneaction.org/sgasupport/?page=about</loc>
+        <lastmod><?php echo date("Y-m-d")?></lastmod>
+    </url>
+    <url>
+        <loc>https://redzoneaction.org/sgasupport/?page=legal</loc>
+        <lastmod><?php echo date("Y-m-d")?></lastmod>
+    </url>
+    <url>
+        <loc>https://redzoneaction.org/sgasupport/?page=faq</loc>
+        <lastmod><?php echo date("Y-m-d")?></lastmod>
+    </url>
+    <url>
+        <loc>https://redzoneaction.org/sgasupport/page=stats</loc>
+        <lastmod><?php echo date("Y-m-d")?></lastmod>
+    </url>
+</urlset>
